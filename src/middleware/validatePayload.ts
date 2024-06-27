8390dbce3004d414f737261d6ada43f82cc665a8
Nathami-n <georgenathan010@gmail.com>
2024-06-27 11:58:41 +0300

    added an any schema to get checked against the schema.parse()

---
+++ src/middleware/validatePayload.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response, query } from "express";
+import { AnyZodObject } from "zod";
+
+const validate =
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+    } catch (e) {
+      return res.status(400).send(e.errors);
+    }
+  };
