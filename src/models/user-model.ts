20731a5d5e91c8b349dfcb31214165c9c07ac872
Nathami-n <georgenathan010@gmail.com>
2024-06-27 13:44:24 +0300

    Add: method to check for validity of password using bcrypt

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -2,6 +2,16 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
 
+export interface IUserDocument extends mongoose.Document {
+    email: string,
+    name: string,
+    password: string,
+    createdAt: Date,
+    updatedAt: Date,
+};
+
+
+
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
@@ -10,7 +20,24 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function(next){
+    let user = this as IUserDocument;
 
+    if(!user.isModified('password')) {
+        return next();
+    };
+
+    const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+    const hash = await bcrypt.hash(user.password, salt);
+
+    user.password = hash;
+    return next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean>{
+    const user = this as IUserDocument;
+    return bcrypt.compare(candidatePassword, user.password).catch(e => false);
+};
 
 const User = mongoose.model("User", userSchema);
 
